a64e965b42241889019d1a4f4f12ccd766ce9e74
E. E. Daisey <121644982+EEDaisey@users.noreply.github.com>
2024-12-21 19:18:45 -0700

    Update and rename Assignment06_CompareDistributionSums to Assignment06_CompareDistributionSums.R

--- Assignment06_CompareDistributionSums.R
+++ Assignment06_CompareDistributionSums.R
@@ -16,8 +16,10 @@ num_times <- 1000 # Can change to lower values.
 # ################################## Analysis ##################################
 # Generate sums of 3 iid Poisson(Mean = 2):
 poisson <- replicate( num_times, sum( rpois( 3, 2 ) ) ) # 3 random samples summed together num_times.
+
 # Directly generated from the resulting Poisson(2 + 2 + 2) = Poisson(6):
 poisson_sum <- rpois( num_times, 6 )
+
 # Sorting & Plotting:
 plot( sort( poisson ), sort( poisson_sum ),
 main = "Comparison of Poisson Sums",
@@ -25,12 +27,15 @@ xlab = "Sum of 3 iid Poisson(2)",
 ylab = "Poisson(6)",
 pch = 20)
 abline( 0, 1 )
+
 # Generate sums of 3 iid Exponential(Rate = 1/4), and then trim:
 exponential <- replicate( num_times, sum( rexp( 3, 1/4 ) ) )
 exponential_trimmed <- exponential[ 51 : 950 ]
+
 # Directly generated from the sums of three Exponential(1/4) random variables, and then trim:
 exponential_sum <- rexp( num_times, 1/4 ) + rexp( num_times, 1/4 ) + rexp(num_times, 1/4)
 exponential_sum_trimmed <- exponential_sum[ 51 : 950 ]
+
 # Sorting & Plotting Against Theoretical Mean Associated With Exponential(Rate = 1/4):
 plot( sort( exponential_trimmed ), sort( exponential_sum_trimmed ),
 main = "Comparison of Exponential Sums",
